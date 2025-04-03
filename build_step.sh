5a3415f826daaedb883c8ef735718b753f19715d
mtyler0606 <mtyler0606@gmail.com>
2025-04-03 18:49:09 -0500

    added start script

---
+++ build_step.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Build script"
+
+# add the commands here
+
+npm run build
+npm run start-prod
--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
+   baseURL: 'http://localhost:3000/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -38,7 +38,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -48,7 +48,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -73,7 +73,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
    webServer: {
      command: 'npm run start',
-     url: 'http://127.0.0.1:3000',
+     url: 'http://localhost:3000',
+     timeout: 30 * 1000,
      reuseExistingServer: !process.env.CI,
    },
 
